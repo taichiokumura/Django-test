6f2315181f90b414accb969b4e12bc7f5c4466ad
himaru <okutaichi2002@gmail.com>
2024-01-24 16:12:27 +0900

    second commit

--- testproject/webapp/views.py
+++ testproject/webapp/views.py
@@ -11,7 +11,7 @@ from .models import Document
 
 def index(request):
     params = {
-        'title': '画像のアップロード',
+        'title': '画像のはりつけ',
         'upload_form': DocumentForm(),
         'id': None,
     }
@@ -27,24 +27,23 @@ def index(request):
  
     return render(request, 'webtestapp/index.html', params)
 
-
-
-# def preview(request, image_id=0):
- 
-#     upload_image = get_object_or_404(Document, id=image_id)
- 
-#     params = {
-#         'title': '画像の表示',
-#         'id': upload_image.id,
-#         'url': upload_image.photo.url
-#     }
- 
-#     return render(request, 'webtestapp/preview.html', params)
-
 from PIL import Image
 from io import BytesIO
 
 def preview(request, image_id=0):
+
+    # # Pillowを使用して画像を読み込む
+    # img = Image.open(image_path)
+    
+    # # 画像を白黒に変換SS
+    # gray_img = img.convert('L')
+
+    # # 白黒画像を保存
+    # bw_image_path = os.path.join(settings.MEDIA_ROOT, 'result_images', 'bw_image.jpg')
+    # gray_img.save(bw_image_path)
+    
+    # bw_url = os.path.join(settings.MEDIA_URL, 'result_images', 'bw_image.jpg')
+
     upload_image = get_object_or_404(Document, id=image_id)
     print(f"Debug: upload_image - {upload_image}")
 
@@ -52,15 +51,15 @@ def preview(request, image_id=0):
     image_path = upload_image.photo.path
     print(f"Debug: Image Path - {image_path}")
 
-    # Pillowを使用して画像を読み込む
-    img = Image.open(image_path)
-    
-    # 画像を白黒に変換
-    gray_img = img.convert('L')
+    # 使用する画像処理ライブラリをPillowからOpenCVに変更
+    img = cv2.imread(image_path)
 
-    # 白黒画像を保存
+    # 画像をグレースケールに変換
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # OpenCVでグレースケール画像を保存
     bw_image_path = os.path.join(settings.MEDIA_ROOT, 'result_images', 'bw_image.jpg')
-    gray_img.save(bw_image_path)
+    cv2.imwrite(bw_image_path, gray_img)
     
     bw_url = os.path.join(settings.MEDIA_URL, 'result_images', 'bw_image.jpg')
 
