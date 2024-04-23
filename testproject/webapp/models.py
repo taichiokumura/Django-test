86538b0fd567de94713b0221867528e84e25db80
himaru <okutaichi2002@gmail.com>
2024-04-23 20:48:55 +0900

    不要ファイル削除

--- testproject/webapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    photo = models.ImageField(upload_to='result_images/', default='SOME STRING')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    gray = models.ImageField(default='Not Set')
