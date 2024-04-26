7d6d5de30c7d07a2b6f9955f412e8e410478e780
himaru <okutaichi2002@gmail.com>
2024-04-26 15:42:34 +0900

    css correction

--- elementary_school_project/webapp/urls.py
+++ elementary_school_project/webapp/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', home.home_header, name='header'),
     path('card_view', card_info.CardListView, name='card'),
     path('cutout_fish', card_create.index, name='index'),
-    path('cutout_fish/<int:image_id>/', card_create.cutout_fish, name='cutout_fish'),
+    # path('cutout_fish/<int:image_id>/', card_create.cutout_fish, name='cutout_fish'),
     # path('home', login.login_card, name='login'),
 ]
